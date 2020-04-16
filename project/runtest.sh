6b3e197bee85d6c47b255ecdbe5b21b9f7058b51
mkrajnak <krajnakmatto@gmail.com>
2020-04-16 03:33:55 +0200

    update runtest

--- project/runtest.sh
+++ project/runtest.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ ! -e /tmp/qecore_setup_done ]; then
+if [ ! -e /tmp/setup_done ]; then
     dnf -y erase python3-behave python2-behave
+    # setup pip deps
     pip3 install -r requirements.txt
-    touch /tmp/qecore_setup_done
+    # setup tesseract
+    dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/Alexander_Pozdnyakov/CentOS_8/
+    rpm --import https://build.opensuse.org/projects/home:Alexander_Pozdnyakov/public_key
+    dnf -y install tesseract
+    touch /tmp/setup_done
 fi
 systemctl stop gdm
 
